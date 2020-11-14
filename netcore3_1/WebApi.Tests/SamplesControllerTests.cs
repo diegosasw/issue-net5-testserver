600e7d5ab991ec7ad50ed16fd8457b674efdff29
Diego Martin <diego.martin@sunnyatticsoftware.com>
2020-11-14 17:41:36 +0100

    Prepared scaffolding with netcore3.1 and net5.0 samples

---
+++ netcore3_1/WebApi.Tests/SamplesControllerTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace WebApi.Tests
+{
+    public class SamplesControllerTests
+    {
+        [Fact]
+        public void Test_Samples_Controller()
+        {
+            var server =
+                new TestServer(
+                    new WebHostBuilder()
+                        .UseStartup<Startup>()
+                        .UseCommonConfiguration()
+                        .UseEnvironment("Test"));
+        }
+
+        [Fact]
+        public void Test_Samples_Controller_With_Mocks()
+        {
+            var server =
+                new TestServer(
+                    new WebHostBuilder()
+                        .UseStartup<Startup>()
+                        .UseCommonConfiguration()
+                        .UseEnvironment("Test")
+                        .ConfigureTestServices(
+                            mockServices =>
+                            {
+
+                            }));
+        }
+    }
+}
---
+++ netcore3_1/WebApi/Controllers/SamplesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SamplesController 
+        : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SamplesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var value = _configuration.GetValue<string>("Foo.Bar");
+            return Ok(value);
+        }
+    }
+}
